68397a2f63d6623b4f87fad0207abc49b799ae82
Abhijeet Pandit <abhijeetpandit7@gmail.com>
2021-02-05 14:45:26 +0530

    Add exception handler

--- Day-26-NATOPhoneticAlphabet/main.py
+++ Day-26-NATOPhoneticAlphabet/main.py
@@ -3,6 +3,12 @@ import pandas as pd
 nato_df = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index,row) in nato_df.iterrows()}
 
-word = input('Enter word here:\n').upper()
-list = [nato_dict[letter] for letter in word if letter!=' ' ]
-print(list)
+while True:
+    word = input('Enter word here:\n').upper()
+    try:
+        list = [nato_dict[letter] for letter in word if letter!=' ' ]
+    except KeyError:
+        print('Sorry, only letter in the alphabet please')
+    else:
+        print(list)
+        break
